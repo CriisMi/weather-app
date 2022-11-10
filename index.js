34227e6a86a7d0fcbfd7c1fe1f13278305d93ebe
Cris <cristinamihalciuc@yahoo.com>
2022-11-10 21:46:54 +0200

    Display data when city is searched

--- index.js
+++ index.js
@@ -3,15 +3,37 @@ async function getWeatherData(cityName) {
         const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ea62a80e0665de9807ac66ce6c9793ce&units=metric`, {mode: 'cors'});
         const mainData = await data.json();
         console.log(mainData.main);
-        return mainData.main;
+        displayData(mainData.main.temp, mainData.main.feels_like, mainData.main.temp_max,
+            mainData.main.temp_min, mainData.main.pressure, mainData.main.humidity);
     } catch (error) {
         console.log(error);
     }
 }
 
+let displayResults = document.querySelector('.results');
+
+function displayDataElement(name, data) {
+    let dataElement = document.createElement('div');
+    dataElement.textContent = `${name}: ${data}`;
+    displayResults.appendChild(dataElement);
+}
+
+function displayData(temp, feelsLikeTemp, tempMax, tempMin, pressure, humidity) {
+    displayResults.textContent = '';
+
+    displayDataElement('Temperature', temp);
+    displayDataElement('Feels like', feelsLikeTemp);
+    displayDataElement('Max temperature', tempMax);
+    displayDataElement('Min temperature', tempMin);
+    displayDataElement('Pressure', pressure);
+    displayDataElement('Humidity', humidity);
+}
+
+
 let searchButton = document.querySelector('.search-button');
 let cityField = document.querySelector('#city');
 searchButton.addEventListener('click', () => {
     let city = cityField.value;
-    getWeatherData(city);
-})
+    let weatherData = getWeatherData(city);
+});
+
