f433f7abc06641b2202a4699f53816773daad9ce
Cris <cristinamihalciuc@yahoo.com>
2022-11-13 10:13:45 +0200

    Display gif based on temperature in celsius

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-async function getWeatherData(cityName) {
+let searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', () => {
+    getWeatherData();
+});
+
+function getUnit() {
+    return (document.querySelector('input[name="temperature"]:checked')).value;
+}
+
+function getCity() {
+    let cityField = document.querySelector('#city');
+    return cityField.value;
+}
+
+/* function getWeatherData() {
+    let params = [26, 11, 12, 13, 14, 15];
+    displayData(params);
+    return params;
+} */
+
+async function getWeatherData() {
     try {
-        const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ea62a80e0665de9807ac66ce6c9793ce&units=metric`, {mode: 'cors'});
+        let tempUnit = getUnit();
+        let cityName = getCity();
+        const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=786f8991633c836ff45fc0636663f50c&units=${tempUnit}`, {mode: 'cors'});
         const mainData = await data.json();
-        console.log(mainData.main);
-        displayData(mainData.main.temp, mainData.main.feels_like, mainData.main.temp_max,
-            mainData.main.temp_min, mainData.main.pressure, mainData.main.humidity);
+        let params = [mainData.main.temp, mainData.main.feels_like, mainData.main.temp_max,
+            mainData.main.temp_min, mainData.main.pressure, mainData.main.humidity];
+        displayData(params);
+        let weather = weatherDescription(mainData.main.temp);
+        getImage(weather);
     } catch (error) {
         console.log(error);
     }
@@ -22,21 +46,49 @@ function displayDataElement(name, data) {
     displayResults.appendChild(dataElement);
 }
 
-function displayData(temp, feelsLikeTemp, tempMax, tempMin, pressure, humidity) {
+function displayData(params) {
     displayResults.textContent = '';
+    let unitVal = getUnit();
+    let unit = '';
+    if (unitVal === 'metric') {
+        unit = ' \xB0' + 'C';
+    } else {
+        unit = ' \xB0' + 'F';
+    }
 
-    displayDataElement('Temperature', temp);
-    displayDataElement('Feels like', feelsLikeTemp);
-    displayDataElement('Max temperature', tempMax);
-    displayDataElement('Min temperature', tempMin);
-    displayDataElement('Pressure', pressure);
-    displayDataElement('Humidity', humidity);
+    displayDataElement('Temperature', params[0] + unit);
+    displayDataElement('Feels like', params[1] + unit);
+    displayDataElement('Max temperature', params[2] + unit);
+    displayDataElement('Min temperature', params[3] + unit);
+    displayDataElement('Pressure', params[4] + ' hPa');
+    displayDataElement('Humidity', params[5] + ' %');
 }
 
 
-let searchButton = document.querySelector('.search-button');
-let cityField = document.querySelector('#city');
-searchButton.addEventListener('click', () => {
-    let city = cityField.value;
-      let weatherData = getWeatherData(city);
-});
+
+
+const img = document.querySelector('img');
+
+function weatherDescription(temp) {
+    let weather = '';
+    if (temp < 5) {
+        weather =  'snow';
+    } else if (temp < 15) {
+        weather =  'autumn';
+    } else if (temp < 30) {
+        weather =  'sunny';
+    } else {
+        weather =  'heatwave';
+    }
+    return weather;
+}
+
+function getImage(weather) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=nqIKYa5RrruB2FzH1wAmvlJXP6KkdaOW&s=${weather}`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        img.src = response.data.images.original.url;
+    });
+}
